eb394d1e7301c22d2f0930e6f5881c3b18f91366
brahian <bsbedoya63@misena.edu.co>
2022-02-22 14:52:57 -0500

    creamos la funciones walking en enter point o de entrada que es el archivo  principal de location

--- wandering.py
+++ wandering.py
@@ -11,4 +11,4 @@ class ComunWandering(Wandering):
         super().__init__(name)
         
     def walk(self):
-        return random.choice([(0, 1),(0, -1), (1, 0), (-1, 0)])
+        return random.choice([(0, 2),(0, -2), (2, 0), (-2, 0)])
