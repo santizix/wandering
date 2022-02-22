feeced0291595fe5b30b0d54cad72dc5a264140c
brahian <bsbedoya63@misena.edu.co>
2022-02-22 14:49:32 -0500

    guardar

--- randomTrack.py
+++ randomTrack.py
@@ -4,3 +4,12 @@ from track import Track
 from location import Location
 
 from bokeh.plotting import figure, output_file, show
+
+def walking(location, wandering, steps):
+    beginning = location.get_location(wandering)
+    
+    for _ in range(steps):
+         location.move_wandering(wandering)
+         
+    return beginning.distance(location.get_location(wandering))
+
